3233850d6203d31d678944c727e5e8195cd2c768
Sanyog Vyawahare <sanvyawahare@gmail.com>
2018-11-06 14:44:34 +0530

    Completed checkResponse() func need to complete updateResponse() func

--- ChatBot.py
+++ ChatBot.py
@@ -1,7 +1,8 @@
 import watson_developer_cloud
-from Connector import Connector as c #Imported my Connector.py code
+from Connector import Connector as c 
 from Global import Global as g
 from Details import Details as d
+import json
 
 service = watson_developer_cloud.AssistantV1(
   username = d.username, 
@@ -11,13 +12,16 @@ service = watson_developer_cloud.AssistantV1(
 workspace_id = d.workspace_id
 
 def checkDetails(response):
+  """
+    This function check if details of child are filled or not and if not then prompt them to fill
+  """
   c.mycursor.execute("SELECT ChildName,Fill FROM PersonalDetails")
   result = c.mycursor.fetchall()
 
   for r_name, val in result:
     if r_name.lower() == response['context']['Name'].lower():
       if not val:
-        response['context']['Details'] = "incomplete"
+        response['context']['Details'] = True
           
   response['context']['Start'] = False
 
@@ -33,6 +37,12 @@ def checkDetails(response):
       response['context']['MotherContact'] = r_name[6]
       response['context']['Subject'] = r_name[7]
 
+def updateDetails(response):
+  """
+    This function updates the personal information provided by the child
+  """
+  pass
+
 def conversation():
   response = service.message(
     workspace_id = workspace_id,
@@ -41,18 +51,21 @@ def conversation():
     },
     context = g.context
   )
-  if response['context']['Start']:
-    checkDetails(response)
-  
   if response['context']['end'] == 'end_conversation': #Conversation ends here
     print("\n".join(''.join(str(cell) for cell in row) for row in response['output']['text'])) 
     g.end_conv = True
 
-  elif response['output']['generic']:
+  elif response['output']['generic']:    # printing response
     print("\n".join(''.join(str(cell) for cell in row) for row in response['output']['text'])) #printing response
     g.context = response['context']
-    print(response['context'])
     g.user_input = input('>> ')
+  
+  if response['context']['Start']:
+    checkDetails(response)
+
+  if not response['context']['Start'] and response['context']['Details']:
+    updateDetails(response)                                                     #Complete this Function next Task
+    #print(json.dumps(response['context'],indent = 2))
   
   if not g.end_conv:
     conversation()
--- Prac.py
+++ Prac.py
@@ -1,11 +1,2 @@
 from Connector import Connector as c
 
-name = "sanyog"
-c.mycursor.execute("SELECT * FROM PersonalDetails")
-result = c.mycursor.fetchall()
-
-
-for c_name in result:
-	
-	
-print("Just for fun")
