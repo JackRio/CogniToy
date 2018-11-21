c234f30035f32d7a0e156b21040be83043f634b8
Sanyog Vyawahare <sanvyawahare@gmail.com>
2018-11-21 19:05:29 +0530

    Web Page Files in Web Page folder added (Index.html and Style.css)

--- ChatBot.py
+++ ChatBot.py
@@ -3,6 +3,10 @@ from Connector import Connector as c
 from Global import Global as g
 from Details import Details as d
 import json
+from flask import Flask, request
+
+app = Flask(__name__)
+
 
 service = watson_developer_cloud.AssistantV1(
   username = d.username, 
@@ -43,22 +47,24 @@ def updateDetails(response):
   """
   pass
 
+@app.route('/conv', methods=['POST'])
 def conversation():
   response = service.message(
     workspace_id = workspace_id,
     input = {
-      'text': g.user_input
+      'text': request.data.input
     },
     context = g.context
   )
   if response['context']['end'] == 'end_conversation': #Conversation ends here
-    print("\n".join(''.join(str(cell) for cell in row) for row in response['output']['text'])) 
+    #print("\n".join(''.join(str(cell) for cell in row) for row in response['output']['text'])) 
     g.end_conv = True
 
   elif response['output']['generic']:    # printing response
-    print("\n".join(''.join(str(cell) for cell in row) for row in response['output']['text'])) #printing response
+    #print("\n".join(''.join(str(cell) for cell in row) for row in response['output']['text'])) #printing response
     g.context = response['context']
-    g.user_input = input('>> ')
+    return g.context
+    #g.user_input = input('>> ')
   
   if response['context']['Start']:
     checkDetails(response)
@@ -67,20 +73,13 @@ def conversation():
     updateDetails(response)                                                     #Complete this Function next Task
     #print(json.dumps(response['context'],indent = 2))
   
-  if not g.end_conv:
-    conversation()
+  #if not g.end_conv:
+    #conversation()
   
 if __name__ == '__main__':
   
-  response = service.message(
-    workspace_id = workspace_id,
-    input = {
-      'text': g.user_input
-    },
-    context = g.context
-  )
+  app.run()
 
-  conversation()
    
 
 
