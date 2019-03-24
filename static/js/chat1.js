a7a6cb2e8c651a79f950b2e29a103ed883fce99e
Sanyog Vyawahare <sanvyawahare@gmail.com>
2019-03-24 18:39:53 +0530

    heroku changes in chat1

--- static/js/chat1.js
+++ static/js/chat1.js
@@ -55,7 +55,7 @@ function appendBotChat(string) {
 
 function getResponse(string) {
 	$.ajax({
-		url: 'http://127.0.0.1:5000/conversation',
+		url: 'https://jrjarvis.herokuapp.com/conversation',
 		headers: {
 			'Content-Type':'application/json'
 		},
@@ -78,7 +78,7 @@ function getResponse(string) {
 
 $(window).on('load', function() {
 	$.ajax({
-		url: 'http://127.0.0.1:5000/start'
+		url: 'https://jrjarvis.herokuapp.com/start'
 	}).always( function(data1) {
 		if(data1 != ''){
 			console.log(data1)
