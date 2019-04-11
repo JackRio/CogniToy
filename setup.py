47116814a5ff28d6fbb3907ea75f1e84bb6058bf
Yj@31197 <yj031197@gmail.com>
2019-04-11 22:52:47 +0530

    chat+setup :1 chat page

--- setup.py
+++ setup.py
@@ -74,10 +74,6 @@ def about():
 
 @app.route('/chat')
 def chat():
-    return render_template('index.html')
-
-@app.route('/chat1')
-def chat1():
     return render_template('chat.html')
 
 @app.route('/chatlog')
