6644444544f40ec9b027f5fadd86f4688e799d68
Sanyog Vyawahare <sanvyawahare@gmail.com>
2019-04-24 12:50:10 +0530

    Merged and removed Js and css files

--- static/js/chat.js
+++ static/js/chat.js
@@ -152,3 +152,19 @@ function volumetoggle(x) {
     }
 
   }
+
+const { body } = document;
+const fullWidth = body.clientWidth;
+const fullHeight = body.clientHeight;
+
+const mouse$ = Rx.Observable
+  .fromEvent(body, 'mousemove')
+  .map(({ offsetX, offsetY }) => ({
+    x: offsetX / fullWidth,
+    y: offsetY / fullHeight
+  }));
+
+mouse$.subscribe(({ x, y }) => {
+  body.style.setProperty('--mouse-x', x);
+  body.style.setProperty('--mouse-y', y);
+});
